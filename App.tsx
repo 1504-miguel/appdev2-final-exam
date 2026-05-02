5ac5a335e3f70c15099a4a1c58d445f50c70f986
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2026-05-02 10:55:34 +0800

    Done all the Signup, Login, Todo screens

--- App.tsx
+++ App.tsx
@@ -1,23 +1,36 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { useState } from "react";
+
 import TodoScreen from "./screens/TodoScreen";
 import LoginScreen from "./screens/LoginScreen";
-import { useState } from "react";
+import SignupScreen from "./screens/SignupScreen";
 
 import { Id } from "./convex/_generated/dataModel";
 
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
-  unsavedChangesWarning: false,
-});
+// IMPORTANT: make sure env variable exists
+const convex = new ConvexReactClient(
+  process.env.EXPO_PUBLIC_CONVEX_URL as string,
+  {
+    unsavedChangesWarning: false,
+  }
+);
 
 export default function App() {
-  const [userId, setUserId] = useState<Id<"users"> | null>(null)
+  const [userId, setUserId] = useState<Id<"users"> | null>(null);
+  const [showSignup, setShowSignup] = useState(false);
 
   return (
     <ConvexProvider client={convex}>
       {userId ? (
         <TodoScreen userId={userId} />
+      ) : showSignup ? (
+        <SignupScreen />
       ) : (
-        <LoginScreen onLogin={(id: Id<"users">) => setUserId(id)} />
+        <LoginScreen
+          onLogin={(id: Id<"users">) => setUserId(id)}
+          
+         
+        />
       )}
     </ConvexProvider>
   );
